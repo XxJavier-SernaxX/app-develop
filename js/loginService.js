f6b6bde536f4df2e5921b62235ca895dc7e2c1ed
Serna <jamaseos77@gmail.com>
2025-04-30 19:58:11 -0500

    segundo commit

--- js/loginService.js
+++ js/loginService.js
@@ -1,38 +1,53 @@
-document.getElementById("formLogin").addEventListener('submit', function(e){e.preventDefault();
+document.getElementById("formLogin").addEventListener('submit' , function(e){
+    e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    let message= ' '
-    let alertType=''
-
-
-
+   login(email, password)
 })
 
-function login (email,password){
-    fetch("htpps://reqres.in/api/login",{
-    method:"POST",
-    headers:{
-        "content-type": "application/json"
-    },
-    body:JSON.stringify({email,password})
+function login(email, password){
+     let message = ""
+     let alerType = ""
+    fetch("https://reqres.in/api/login",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "x-api-key": "reqres-free-v1"
+        },
+        body: JSON.stringify({email, password})
     })
-     .then((data)=>{
-        alertType = 'success'
-        message='Inicio de Sesion correcto';
-        
-        console.log('responde bien')
-     })
-     .catch((error)=>{
-        alertType = 'danger'
-        message='correo o contraseña incorrecto';
-        console.error(error)
-     })
 
-     let alert = `
-     <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-    ${message}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>
-  `;
-    document.getElementById('alert').innerHTML=alert;
+    .then((response) => {
+        if(response.status === 200){
+            alerType = "success"
+            message = 'Incio de secion exitosa'
+            console.log("Responde bien"+ response)
+            alertbuilder(alerType,message)
+            setTimeout(() => {
+                location.href = "admin/dashboard.html"
+            location.href= 'admin/dashboard.html'
+            },2000)//2000
+
+        }else{
+             alerType = "danger"
+             message = "correo o contraseña incorrecta"
+             alertbuilder(alerType,message)
+
+        }
+       
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+   
+
+    function alertbuilder(alerType, message){
+        let alert = `
+    <div class="alert alert-${alerType} alert-dismissible fade show" role="alert">
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div> 
+    `;
+    document.getElementById("alert").innerHTML = alert
 }
